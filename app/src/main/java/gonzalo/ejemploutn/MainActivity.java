9f5f55a03460d9ef5c169aeaf566e5b80082d761
Gonzalo Benoffi <benoffi11@gmail.com>
2016-09-29 15:31:41 -0300

    Componentes basicos

--- app/src/main/java/gonzalo/ejemploutn/MainActivity.java
+++ app/src/main/java/gonzalo/ejemploutn/MainActivity.java
@@ -1,7 +1,14 @@
 package gonzalo.ejemploutn;
 
+import android.content.Context;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        generateNoteOnSD(getApplicationContext(),"holas.txt","texto");
+
+    }
+
+    public void generateNoteOnSD(Context context, String sFileName, String sBody) {
+        try {
+            File root = new File(Environment.getExternalStorageDirectory(), "Notes");
+            if (!root.exists()) {
+                root.mkdirs();
+            }
+            File gpxfile = new File(root, sFileName);
+            FileWriter writer = new FileWriter(gpxfile);
+            writer.append(sBody);
+            writer.flush();
+            writer.close();
+            Toast.makeText(context, "Saved", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
