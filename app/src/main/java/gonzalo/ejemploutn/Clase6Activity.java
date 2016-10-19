18c00d38b5ae35222023ec8176f5512f44707033
Gonzalo Benoffi <benoffi11@gmail.com>
2016-10-19 17:38:49 -0300

    Comentarios clases 19/10

--- app/src/main/java/gonzalo/ejemploutn/Clase6Activity.java
+++ app/src/main/java/gonzalo/ejemploutn/Clase6Activity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -14,56 +15,74 @@ import android.widget.Toast;
 
 public class Clase6Activity extends Activity
 {
+    //espacio de declaracion de variables tanto de XML como auxiliares
     EditText editText_name;
     EditText editText_pass;
     Button button_login;
 
     String password = "gonzalo";
 
+    //metodo que se invoca AUTOMATICAMENTE al crearse la actividad
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        //establezco la vista
         setContentView(R.layout.lay_login);
+        //invoco a mis funciones
         findViews();
         setEvents();
     }
 
+    //aqui hago la relacion entre los componentes XML con mis variables JAVA
     void findViews()
     {
+
+        //una variable por cada control. el nombre de las mismas deberia ser igual al id del componente XML...
         editText_name = (EditText)findViewById(R.id.editText_name);
         editText_pass = (EditText)findViewById(R.id.editText_pass);
         button_login = (Button)findViewById(R.id.button_login);
     }
 
+    //seteo de eventos, en este caso solo del boto
     void setEvents()
     {
-        button_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if (passwordCorrecto())
-                {
-                    Toast.makeText(getApplicationContext(),"Ingreso correcto",Toast.LENGTH_SHORT).show();
-                    if (editText_name.getText().toString().length()==0)
-                    {
-                        Toast.makeText(getApplicationContext(),"No ingreso el nombre",Toast.LENGTH_SHORT).show();
-                    }
-                    else
-                    {
-                        Intent intent = new Intent(Clase6Activity.this, Clase6BisActivity.class);
-                        intent.putExtra("nombre", editText_name.getText().toString());
-                        startActivity(intent);
-                    }
-                }
-                else
-                {
-                    Toast.makeText(getApplicationContext(),"Ingreso incorrecto",Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+       button_login.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view)
+           {
+               //si no tengo error
+               if (passwordCorrecto())
+               {
+                   //estoy mostrando un cartelito. el primero es....
+                   Toast.makeText(getApplicationContext(),getString(R.string.texto1),Toast.LENGTH_SHORT).show();
+                   String nombre;
+                   //copio el contenido de mi caja de texto a una variable local...
+                   nombre = editText_name.getText().toString();
+                   //evaluo si el nombre tiene caracteres o no...
+                   if (nombre.length()==0)
+                   {
+                       Toast.makeText(getApplicationContext(),"No ingreso el nombre",Toast.LENGTH_SHORT).show();
+                   }
+                   else
+                   {
+                       //lanzo otra actividad con el NOMBRE como extra...recordar camion,acoplado y cajitas
+                       Intent intent = new Intent(Clase6Activity.this, Clase6BisActivity.class);
+                       intent.putExtra("nombre", editText_name.getText().toString());
+                       startActivity(intent);
+                   }
+               }
+               else
+               {
+                   //muestro de mensaje de error
+                   Toast.makeText(getApplicationContext(),"Ingreso incorrecto",Toast.LENGTH_SHORT).show();
+               }
+           }
+       });
     }
 
+    //funcion que retorna verdadero o falso segun si la caja de texto tiene lo que guarde en la variable java llamada password
+    //equals es un metodo de java que evalua un String (cadena de caracteres) con otra
     boolean passwordCorrecto()
     {
         if (password.equals(editText_pass.getText().toString()))
@@ -74,5 +93,14 @@ public class Clase6Activity extends Activity
         {
             return  false;
         }
+    }
+
+    // este metodo se ejecuta cuando vuelvo por segunda vez a esta actividad
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //limpio la caja de texto
+        editText_pass.getText().clear();
+
     }
 }
--- app/src/main/java/gonzalo/ejemploutn/Clase6BisActivity.java
+++ app/src/main/java/gonzalo/ejemploutn/Clase6BisActivity.java
@@ -17,27 +17,47 @@ public class Clase6BisActivity extends Activity
 {
     TextView textView_nombre;
     String nombreRecibido = "";
+    int edadRecibida ;
+
+
+    void findViews()
+    {
+        //nada raro over here solo guardo en mi variable java el componente XML
+       textView_nombre = (TextView)findViewById(R.id.textView_nombre);
+    }
+
+    void setUI()
+    {
+        //utilizando el seteo de una propiedad texto del textview, le asigno el nombre que recibi
+        textView_nombre.setText("Mi nombre es: "+nombreRecibido);
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lay_login_ok);
+        //levanto camion
         Intent intentoRecibido = getIntent();
+        //levanto acoplado
         Bundle extrasRecibidos = intentoRecibido.getExtras();
+        //levanto UNA cajita con NOMBRE como clave...recordar usar el metodo getString si envie una cadena de carateres o getInt si es un numero
         nombreRecibido = extrasRecibidos.getString("nombre");
+        edadRecibida = extrasRecibidos.getInt("edad");
+        //invocacion a mis funciones...
         findViews();
         setUI();
     }
-
-    void findViews()
-    {
-       textView_nombre = (TextView)findViewById(R.id.textView_nombre);
-    }
-
-    void setUI()
-    {
-        textView_nombre.setText("Mi nombre es: "+nombreRecibido);
-    }
-
 }
